945f49308dc765d3156ce1e5a244c833a48b50de
Jun Huang <jh123.jun@gmail.com>
2024-02-26 12:25:20 -0500

    Modified the hero section slightly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/app/lib/ui/home-page/navbar"
-import {poppins} from "@/app/lib/ui/fonts"
+import { poppins } from "@/app/lib/ui/fonts"
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,26 +9,29 @@ export default function Home() {
       <Navbar />
 
       {/* Hero Section */}
-      <div className="flex gap-2 text-white items-center justify-evenly">
-        <div className="w-1/2 text-wrap">
-          <h1 className="text-3xl md:text-4xl xl:text-6xl mb-16 mt-6 lg:mt-0">
-            Manage your <div className="mt-2"></div> 
-            expenses anywhere <div className="mt-2"></div> 
-            in real time
-          </h1>
-          <Link href='/login' className="px-4 py-2 rounded bg-emerald-500	">Get started</Link>
+      <div className="flex flex-col items-center"> {/* Added items-center class */}
+        <div className="flex gap-2 text-white items-center justify-evenly">
+          <div className="w-1/2 text-wrap">
+            <h1 className="text-3xl md:text-4xl xl:text-6xl mb-16 mt-6 lg:mt-0">
+              Manage your <div className="mt-2"></div> 
+              expenses anywhere <div className="mt-2"></div> 
+              in real time
+            </h1>
+            <Link href='/login' className="px-4 py-2 rounded bg-emerald-500	">Get started</Link>
+          </div>
+          
+          <Image 
+            src="/hero-image.png"
+            alt="hero image"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '25%', height: 'auto' }}
+          />
         </div>
-        
-        <Image 
-          src="/hero-image.png"
-          alt="hero image"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: '25%', height: 'auto' }}
-        />
+        <hr className="w-4/5 mt-4 mx-auto"/>
       </div>
-      <hr className="w-4/5 mt-4 mx-auto"/>
+      
     </main>
   );
 }
