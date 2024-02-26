c2548d6ea461b03f1b4a8a691402d7460a05810a
Jun Huang <jh123.jun@gmail.com>
2024-02-26 12:07:44 -0500

    Revert "Added navbar and hero section to the landing page"
    This reverts commit 7de1323f88037192a9e059bdf7dfa2cebb06d465.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,8 @@
-import Navbar from "@/app/lib/ui/home-page/navbar"
-import {poppins} from "@/app/lib/ui/fonts"
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={`${poppins.className} flex flex-col min-h-screen bg-gray-950`}>
-      <Navbar />
-
-      {/* Hero Section */}
-      <div className="flex gap-2 text-white items-center justify-evenly">
-        <div className="w-1/2 text-wrap">
-          <h1 className="text-3xl md:text-4xl xl:text-6xl mb-16 mt-6 lg:mt-0">
-            Manage your <div className="mt-2"></div> 
-            expenses anywhere <div className="mt-2"></div> 
-            in real time
-          </h1>
-          <Link href='/login' className="px-4 py-2 rounded bg-emerald-500	">Get started</Link>
-        </div>
-        
-        <Image 
-          src="/hero-image.png"
-          alt="hero image"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: '25%', height: 'auto' }}
-        />
-      </div>
-      <hr className="w-4/5 mt-4 mx-auto"/>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>LANDING PAGE 1</div>
     </main>
   );
 }
