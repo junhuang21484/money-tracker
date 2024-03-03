e6aa266edacd08dc95a876d889cbbdc43de573b2
Jun Huang <jh123.jun@gmail.com>
2024-03-03 17:18:42 -0500

    Added various functions to fetch/insert data

--- src/app/lib/data/plaidConnections.js
+++ src/app/lib/data/plaidConnections.js
@@ -1,5 +1,20 @@
 import connection from "./connector";
 
+export function getPlaidConnectionByInstitutionName(userID, institutionName) {
+  const sql = `SELECT * FROM plaidConnections WHERE user_id=? AND institution_name=?`;
+  const values = [userID, institutionName];
+
+  return new Promise((resolve, reject) => {
+    connection.query(sql, values, (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+
+      resolve(results);
+    });
+  })
+}
+
 export function insertNewPlaidConnection(itemID, accessToken, userID, institutionName) {
   const sql = `INSERT INTO plaidConnections (item_id, access_token, user_id, institution_name, transaction_cursor) VALUES (?, ?, ?, ?, ?)`;
   const values = [itemID, accessToken, userID, institutionName, null];
