9b1ad01da73bfb6178190e951012243bed46b305
Jun Huang <jh123.jun@gmail.com>
2024-03-03 20:08:00 -0500

    Revert "Merge branch 'dashboard' into account"
    This reverts commit 806fd182e385cfc5718f214cf9fcd5bceccab7c3, reversing
    changes made to a90cf574384bac333effa554dfc77b56592f7d2b.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { Inter } from "next/font/google";
 import { poppins } from "@/app/lib/ui/fonts"
 import "./globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
