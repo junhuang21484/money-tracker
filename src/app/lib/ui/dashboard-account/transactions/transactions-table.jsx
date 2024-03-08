706149e45904d67c3c8deaf880ed78496d1240cc
Jun Huang <jh123.jun@gmail.com>
2024-03-08 12:46:02 -0500

    Added transaction table to its own component

---
+++ src/app/lib/ui/dashboard-account/transactions/transactions-table.jsx
@@ -0,0 +1,67 @@
+export default function TransactionsTable() {
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-400">
+        <thead className="text-xs uppercase bg-gray-700 text-emerald-500">
+          <tr>
+            <th scope="col" className="p-4">
+              <div className="flex items-center">
+                <input
+                  id="checkbox-all-search"
+                  type="checkbox"
+                  className="w-4 h-4 rounded focus:ring-blue-600 ring-offset-gray-800 focus:ring-offset-gray-800 focus:ring-2 bg-gray-700 border-gray-600"
+                />
+                <label for="checkbox-all-search" className="sr-only">
+                  checkbox
+                </label>
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Date
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Category
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Amount
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-gray-800 hover:bg-gray-600 text-white">
+            <td className="w-4 p-4">
+              <div className="flex items-center">
+                <input
+                  id="checkbox-table-3"
+                  type="checkbox"
+                  className="w-4 h-4  rounded focus:ring-blue-600 ring-offset-gray-800 focus:ring-offset-gray-800 focus:ring-2 bg-gray-700 border-gray-600"
+                />
+                <label for="checkbox-table-3" className="sr-only">
+                  checkbox
+                </label>
+              </div>
+            </td>
+            <th className="px-6 py-4 font-medium">Spotify</th>
+            <td className="px-6 py-4">March 1st, 2024</td>
+            <td className="px-6 py-4">Music</td>
+            <td className="px-6 py-4">$12.99</td>
+            <td className="px-6 py-4">
+              <a
+                href="#"
+                className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                Edit
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
