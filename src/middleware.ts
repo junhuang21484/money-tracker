185ed658e8199dcc60113f83c1f376d36871fc49
Jun Huang <jh123.jun@gmail.com>
2024-03-18 10:59:17 -0400

    Added middleware to the application

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
  
-export function middleware(request) {
-    const path = request.nextUrl.pathname
-    
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname    
     const isPublicPath = path === '/login' || path === '/signup'
 
-    const token = request.cookies.get('token')?.value || ''
+    const token = request.cookies.get('token')?.value || null
 
     if (isPublicPath && token) 
         return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
