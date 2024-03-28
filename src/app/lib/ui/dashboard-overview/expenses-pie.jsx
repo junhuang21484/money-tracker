6fdbb346cecfdfd035eba6e3495631e0e2f07741
Yicheng Wang <wyc0914@gmail.com>
2024-03-27 20:57:07 -0400

    UI update for PieChart in Overview

--- src/app/lib/ui/dashboard-overview/expenses-pie.jsx
+++ src/app/lib/ui/dashboard-overview/expenses-pie.jsx
@@ -1,37 +1,56 @@
 import React from 'react';
-import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer, Text } from 'recharts';
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#A28FD0', '#D0D0D0'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const dataForPieChart = [
-    { name: 'Category A', value: 400 },
-    { name: 'Category B', value: 300 },
-    { name: 'Category C', value: 300 },
-    { name: 'Category D', value: 200 },
-  ];
+  { name: 'Category A', value: 100 },
+  { name: 'Category B', value: 150 },
+  { name: 'Category C', value: 300 },
+  { name: 'Category D', value: 200 },
+];
+
+const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius + 15;
+    const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+    
+    return (
+      <Text 
+        x={x} 
+        y={y} 
+        fill={COLORS[index % COLORS.length]} 
+        textAnchor={x > cx ? 'start' : 'end'} 
+        dominantBaseline="central">
+        {`${dataForPieChart[index].name} (${(percent * 100).toFixed(0)}%)`}
+      </Text>
+    );
+  };
 
 export default function ExpensesPie() {
   return (
-    <ResponsiveContainer width="100%" height={300}>
-    <h1 className='text-xl text-center'>Expenses By Category Overall</h1>
-      <PieChart>
-        <Pie
-          data={dataForPieChart}
-          cx="50%"
-          cy="50%"
-          labelLine={false}
-          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
-        >
-          {dataForPieChart.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-        <Tooltip />
-        <Legend />
-      </PieChart>
-    </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            data={dataForPieChart}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius="80%"
+            innerRadius="60%"
+            fill="#8884d8"
+            paddingAngle={5}
+            dataKey="value"
+          >
+            {dataForPieChart.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend align="center" verticalAlign="bottom" layout="horizontal" wrapperStyle={{ bottom: -10 }} />
+        </PieChart>
+      </ResponsiveContainer>
   );
 }
