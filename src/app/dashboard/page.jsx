bd806292bd0d944bd8824c4b8445b1415cd40b67
Yicheng Wang <wyc0914@gmail.com>
2024-03-27 20:28:31 -0400

    Create UI design for the graphs in overview page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,3 +1,8 @@
+"use client"
+
+import OverviewGraph from "@/app/lib/ui/dashboard-overview/balance-over-time";
+import ExpensesPie from "@/app/lib/ui/dashboard-overview/expenses-pie";
+
 export default function DashboardPage() {
   const sectionHeaderStyling = "text-xl font-bold md:text-2xl";
   return (
@@ -6,8 +11,12 @@ export default function DashboardPage() {
         <h1 className={sectionHeaderStyling}>Overview</h1>
       </div>
 
-      <div className="rounded-lg border-2 p-4 border-gray-500">
+      <div className="rounded-lg border-2 p-12 border-gray-500">
         <h1 className={sectionHeaderStyling}>Analytics</h1>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <OverviewGraph />
+        <ExpensesPie />
+        </div>
       </div>
 
       <div className="rounded-lg border-2 p-4 border-gray-500">
---
+++ src/app/lib/ui/dashboard-overview/expenses-pie.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#A28FD0', '#D0D0D0'];
+
+const dataForPieChart = [
+    { name: 'Category A', value: 400 },
+    { name: 'Category B', value: 300 },
+    { name: 'Category C', value: 300 },
+    { name: 'Category D', value: 200 },
+  ];
+
+export default function ExpensesPie() {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+    <h1 className='text-xl text-center'>Expenses By Category</h1>
+      <PieChart>
+        <Pie
+          data={dataForPieChart}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {dataForPieChart.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
