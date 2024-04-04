90ba6bb6c86475f9ee32f2d0511941559750735e
Abdelhak Ouznadji <abdelhakouznadji@Abdelhaks-MBP.fios-router.home>
2024-04-04 05:27:47 -0400

    login/register/landing page ui improvement

--- src/app/lib/ui/home-page/navbar.jsx
+++ src/app/lib/ui/home-page/navbar.jsx
@@ -2,19 +2,26 @@ import Link from "next/link";
 import { cookies } from "next/headers";
 
 export default function Navbar() {
-    const cookieStore = cookies()
-    const isLoggedIn = cookieStore.get("token")
-    return (
-        <nav className="flex justify-around h-16 items-center">
-            <Link href='/' className="text-emerald-500 font-extrabold text-2xl lg:text-4xl w-1/2">
-                Money Minder
-            </Link>
-            
-            <div className="flex gap-2">
-                <Link href={isLoggedIn ? "/dashboard": "/login"} className="text-emerald-500 bg-slate-800 py-2 px-4 rounded hover:bg-slate-600">
-                    {isLoggedIn ? "Dashboard" : "Login"}
-                </Link>
-            </div>
-        </nav>
-    )
+  const cookieStore = cookies();
+  const isLoggedIn = cookieStore.get("token");
+  return (
+    <nav className="fixed mx-auto top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-98">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-12 py-2">
+        <Link
+          href="/"
+          className="text-2xl md:text-4xl text-white font-semibold"
+        >
+          MoneyMinder
+        </Link>
+        <div className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+          <Link
+            href={isLoggedIn ? "/dashboard" : "/login"}
+            className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2 px-6 rounded"
+          >
+            {isLoggedIn ? "Dashboard" : "Login"}
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+const colors = require("tailwindcss/colors");
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      ...colors,
+      primary: colors.green,
+      secondary: colors.green,
     },
   },
   plugins: [],
